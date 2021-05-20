35fe2c719b0a73de7e2eb26ebe9b50c4575f33c1
Willem Van Onsem <vanonsem.willem@gmail.com>
2021-05-20 22:59:14 +0200

    added meta data to the pdf files

--- make.sh
+++ make.sh
@@ -26,6 +26,8 @@ buildpdf () {
     makeindex "$bn" || true
   done
   rm *.adx
+  pdftk "$bn.pdf" update_info meta.txt output "out-$bn.pdf"
+  mv -f "out-$bn.pdf" "$bn.pdf"
 }
 
 
@@ -48,13 +50,22 @@ done
 cd out
 echo "afvalwoordenboek.django-antipatterns.com" > CNAME
 echo '<!DOCTYPE html><meta charset="utf-8"><title>Redirecting to /afvalwoordenboek_dark.pdf</title><meta http-equiv="refresh" content="0; URL=/afvalwoordenboek_dark.pdf"><link rel="canonical" href="/afvalwoordenboek_dark.pdf">' > index.html
-
+cat > meta.txt <<EOF
+InfoKey: Title
+InfoValue: Afval-sorteer-woordenboek
+InfoKey: Subject
+InfoValue: recycleren van afval
+InfoKey: Author
+InfoValue: Willem Van Onsem and Lindsey Louwyck
+InfoKey: Keywords
+InfoValue: afval, recyclage, pmd
+EOF
 
 for fn in *.tex; do
   buildpdf "$fn" &
 done
 wait
 
-rm *.aux *.glo *.idx *.ilg *.ind *.ist *.log *.out *.tex *.sty *.cls *.adx *.and *.ttf
+rm *.aux *.glo *.idx *.ilg *.ind *.ist *.log *.out *.tex *.sty *.cls *.adx *.and *.ttf meta.txt
 
 exit 0
